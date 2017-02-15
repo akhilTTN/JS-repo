92bfe35f5cdc50d6313bfd4e3ce023b1c1ae6736
akhilTTN <akhil@tothenew.com>
2017-02-15 17:39:19 +0530

    commited index and q1

---
+++ q1.js
@@ -0,0 +1,5 @@
+var amount=parseInt(prompt("Enter amount: "));
+            var rate=parseInt(prompt("Enter interest rate: "));
+            var years=parseInt(prompt("Enter number of years: "));
+            var SI=(amount*rate*years)/100;
+            alert("Simple intrest is"+SI);
